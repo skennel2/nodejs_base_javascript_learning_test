2293ed1cdc94ffb6705edbc0288ef653e457e93b
skennel2 <skennel2@gmail.com>
2019-04-05 11:34:21 +0900

    모듈 테스트

---
+++ test/module/module.js
@@ -0,0 +1,3 @@
+exports.someModule = {
+    someValue : 10
+}
---
+++ test/moduleTest.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+var md = require("./module/module");
+
+describe('모듈 테스트', function(){
+    it('module.js 모듈 불러오기 테스트', function () {
+        assert.equal(10, md.someModule.someValue);
+    });
+})
