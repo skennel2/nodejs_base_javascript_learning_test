91194e94964ce0997789f4113ee9fd5cd2667796
skennel2 <skennel2@gmail.com>
2019-04-06 15:30:31 +0900

    mocha hook

--- test/mochaTest.js
+++ test/mochaTest.js
@@ -1,5 +1,32 @@
 var assert = require("assert"); //nodejs에서 제공하는 assert 모듈
 
+describe('mocha 사용법', function(){
+	let status = "ready";
+	before(function(){
+		status = 'before';
+		console.log(status);
+	});
+
+	after(function(){
+		status = 'after';
+		console.log(status);
+	});
+
+	beforeEach(function(){
+		status = 'beforeEach';
+		console.log(status);
+	});
+
+	afterEach(function(){
+		status = 'afterEach';
+		console.log(status);
+	});
+
+	it('run mocha hook', function(){
+
+	})
+});
+
 describe('Array 테스트', function() {
 	describe('indexOf() 메서드', function () {
 		it('값이 없을 때에는 -1 을 리턴함', function () {
