e4fd33db968b6ba28cadbc39839f4ca6e0cc252a
skennel2 <skennel2@gmail.com>
2020-01-20 15:00:04 +0900

    일반 함수와 화살표 함수에서의 this의 차이

--- test/this.js
+++ test/this.js
@@ -1,42 +1,62 @@
 var assert = require('assert');
 
-/**
- * TODO: javascript의 this와 node.js에서의 this는 차이가 있는듯하다.
- */
-describe('this 테스트', function(){
-    it('this', function(){
+describe('this 테스트', () => {
+    it('화살표함수와 일반 함수에서 this의 차이', () => {
         // this 는 일반적으로는 메소드를 호출한 객체가 저장되어 있는 속성이다.
-        var name = 'kevin'
+        this.name = 'kevin'
 
         var jane = {
-            name : 'jane',
-            age  : 27,
-            job  : 'designer',
-            getName : function(){
+            name: 'jane',
+            age: 27,
+            job: 'designer',
+            getName: function () {
                 return this.name;
             },
-            setName : function(name){
+            setName: function (name) {
+                this.name = name;
+            },
+            getNameArrow: () => {
+                return this.name;
+            },
+            setNameArrow: (name) => {
                 this.name = name;
             }
         };
-        
-        jane.setName('Jane');
-        
-        assert.equal('Jane', jane.getName());
-        assert.equal('Jane', jane.name);
-        assert.equal('kevin', name);
+
+        // 화살표 함수와 일반 
+        jane.setName('Joe');
+        assert.equal('Joe', jane.getName());
+        assert.equal('Joe', jane.name);
+
+        jane.setNameArrow('Marry');
+        assert.equal('Joe', jane.getName());
+        assert.equal('Marry', this.name);
+    });
+
+    it('화살표함수 내부의 this의 의미', () => {
+        let someFunction = () => {
+            this.age = 10;
+            assert.equal(10, this.age);
+        }
+
+        someFunction();
+
+        assert.equal(10, this.age);
+
     });
 
     /**
-     * TODO: 
+     * 위 예제와 결과에 차이가 없다.
      */
-    it('this2', function(){
-        function someFunction(){
+    it('function 함수 내부의 this의 의미', () => {
+        function someFunction() {
             this.age = 10;
-            assert.equal(10, this.age, 'function내 에서의 this의 의미');
+            assert.equal(10, this.age);
         }
-    
+
         someFunction();
+
         assert.equal(10, this.age);
+
     });
 });
